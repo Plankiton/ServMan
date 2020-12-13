8218de8e13709da4d3751afc66a3bfe13ac4fc2e
Plankiton <pl4nk1ton@gmail.com>
2020-12-12 23:39:54 -0300

    Adiciona rotas para gerenciar as fazendas

--- servman-api/farm/farm.go
+++ servman-api/farm/farm.go
@@ -12,20 +12,225 @@ import (
 
 type Addr struct {
     Street       string `json:"street,omitempty"`
-    Country      string `json:"country,omitempty"`
+    State        string `json:"state,omitempty"`
     Number       string `json:"number,omitempty"`
-    Code         string `json:"code,omitempty"`
+    Code         string `json:"cep,omitempty"`
     City         string `json:"city,omitempty"`
-    Neightbourn  string `json:"neightbourn,omitempty"`
+    Neightbourn  string `json:"neightborhood,omitempty"`
 }
 
 type Farm struct {
-    ID      string `json:"id,omitempty"`
-    Address  *Addr `json:"address,omitempty"`
-    Name    string `json:"name,omitempty"`
+    PersonId  string `json:"client_id,omitempty"`
+    ID        string `json:"id,omitempty"`
+    Address   *Addr  `json:"address,omitempty"`
+    Name      string `json:"name,omitempty"`
 
     CreateTime time.Time `json:"created_at,omitempty"`
     UpdateTime time.Time `json:"updated_at,omitempty"`
 }
 
 
+var farms []Farm
+
+// GetPeople mostra todos os contatos da variável farms
+func GetFarms(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+
+    var founds []Farm
+    for _, f := range(farms) {
+        if (f.PersonId == params["id"]){
+            founds = append(founds, f)
+        }
+    }
+
+    json.NewEncoder(w).Encode(founds)
+}
+
+// GetFarm mostra apenas um contato
+func GetFarm(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    for _, item := range farms {
+        if item.ID == params["id"] {
+            json.NewEncoder(w).Encode(item)
+            return
+        }
+    }
+    json.NewEncoder(w).Encode(&Farm{})
+}
+
+// CreateFarm cria um novo contato
+func CreateFarm(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+
+    var body util.Request
+    json.NewDecoder(r.Body).Decode(&body)
+
+    if len(body.Data) == 0 {
+        w.WriteHeader(400)
+
+        json.NewEncoder(w).Encode(util.Response{
+            Message: "The data sent is invalid!"+
+                     `(must be {"data": "..."})`,
+            Code: "BadRequest",
+            Type: "error",
+            Data: nil,
+        })
+
+        return
+    }
+
+
+    farm := Farm {
+        Address: &Addr{},
+        PersonId: params["id"],
+    }
+
+    r_addr, err := http.Get(fmt.Sprintf("https://brasilapi.com.br/api/cep/v1/%s", body.Data["cep"]))
+    if err == nil {
+        json.NewDecoder(r_addr.Body).Decode(&farm.Address)
+    }
+
+    for i, prop := range(body.Data) {
+        switch i {
+        case "name":
+            farm.Name = prop
+        case "cep":
+            if farm.Address.Code == "" {
+                farm.Address.Code = prop
+            }
+        case "street":
+            if farm.Address.Street == "" {
+                farm.Address.Street = prop
+            }
+        case "number":
+            if farm.Address.Number == "" {
+                farm.Address.Number = prop
+            }
+        case "neighborhood":
+            if farm.Address.Neightbourn == "" {
+                farm.Address.Neightbourn = prop
+            }
+        case "state":
+            if farm.Address.State == "" {
+                farm.Address.State = prop
+            }
+        case "city":
+            if farm.Address.City == "" {
+                farm.Address.City = prop
+            }
+        }
+    }
+
+
+    for _, item := range farms {
+        if item.ID == farm.ID {
+            w.WriteHeader(403)
+
+            json.NewEncoder(w).Encode(util.Response{
+                Message: "The user already exists!",
+                Code: "AlreadyExists",
+                Type: "error",
+                Data: nil,
+            })
+
+            return
+        }
+    }
+
+    farm.CreateTime = time.Now()
+    farm.UpdateTime = time.Now()
+
+    farms = append(farms, farm)
+    res := util.Response {
+        Type:    "sucess",
+        Message: "Farm created!",
+        Code:    "CreatedFarm",
+        Data:    farm,
+    }
+
+    json.NewEncoder(w).Encode(res)
+}
+
+func UpdateFarm(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+
+    var body util.Request
+    json.NewDecoder(r.Body).Decode(&body)
+
+    for index, item := range farms {
+        if item.ID == params["id"] {
+            farm := farms[index]
+            not_set := false
+
+            for i, prop := range(body.Data) {
+                switch i {
+                case "name":
+                    farm.Name = prop
+                case "cep":
+                    farm.Address.Code = prop
+
+                    r_addr, err := http.Get(fmt.Sprintf("https://brasilapi.com.br/api/cep/v1/%s", body.Data["cep"]))
+                    if err == nil {
+                        json.NewDecoder(r_addr.Body).Decode(&farm.Address)
+                    }
+
+                case "street":
+                    farm.Address.Street = prop
+                case "number":
+                    farm.Address.Number = prop
+                case "neighborhood":
+                    farm.Address.Neightbourn = prop
+                case "state":
+                    farm.Address.State = prop
+                case "city":
+                    farm.Address.City = prop
+                default:
+                    not_set = true
+                }
+            }
+
+
+            if !not_set {
+                farm.UpdateTime = time.Now()
+
+                farms[index] = farm
+                json.NewEncoder(w).Encode(util.Response{
+                    Message: fmt.Sprintf("Farm %s did updated!", farm.Name),
+                    Code: "UpdatedFarm",
+                    Type: "sucess",
+                    Data: farm,
+                })
+
+                return
+            }
+        }
+    }
+
+    w.WriteHeader(404)
+
+    json.NewEncoder(w).Encode(util.Response{
+        Message: "The farm not found!",
+        Code: "NotFound",
+        Type: "error",
+    })
+}
+
+// DeleteFarm deleta um contato
+func DeleteFarm(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    for index, item := range farms {
+        if item.ID == params["id"] {
+            farms = append(farms[:index], farms[index+1:]...)
+            break
+        }
+        json.NewEncoder(w).Encode(farms)
+    }
+}
+
+func AppendFarm(p Farm) {
+    farms = append(farms, p)
+}
+
+func Farms() []Farm {
+    return farms
+}
--- servman-api/main.go
+++ servman-api/main.go
@@ -6,12 +6,12 @@ import (
     "net/http"
 
     "./user"
+    "./farm"
 )
 
 // função principal para executar a api
 func main() {
     router := mux.NewRouter()
-    router.Name("ServMan API")
 
     p := user.Person{
         ID: "1",
@@ -25,19 +25,20 @@ func main() {
     p.SetPass("joao")
     user.AppendPerson(p)
 
-    p = user.Person{
-        ID: "2",
-        Password: &user.Pass{},
-        Document: &user.Doc{
-            Type: "cpf",
-            Value: "123456789",
+    f := farm.Farm{
+        ID: "1",
+        PersonId: "1",
+        Name: "Maria de cristo",
+        Address: &farm.Addr{
+            Street: "Joao de melo paia",
+            City:   "Sao Paulo",
+            State:  "SP",
+            Code:   "123233445",
         },
-        Name: "Joao Da Silva",
-        Type: "root",
     }
-    p.SetPass("joao")
-    user.AppendPerson(p)
+    farm.AppendFarm(f)
 
+    // User
     router.HandleFunc("/user", user.GetPeople).Methods("GET")
 
     router.HandleFunc("/user", user.CreatePerson).Methods("POST")
@@ -45,6 +46,15 @@ func main() {
     router.HandleFunc("/user/{id}", user.DeletePerson).Methods("DELETE")
     router.HandleFunc("/user/{id}", user.UpdatePerson).Methods("POST")
     log.Output(2, "Routing /user - User operations")
+
+    // Farm
+    router.HandleFunc("/user/{id}/farm", farm.GetFarms).Methods("GET")
+
+    router.HandleFunc("/user/{id}/farm", farm.CreateFarm).Methods("POST")
+    router.HandleFunc("/user/farm/{id}", farm.GetFarm).Methods("GET")
+    router.HandleFunc("/user/farm/{id}", farm.DeleteFarm).Methods("DELETE")
+    router.HandleFunc("/user/farm/{id}", farm.UpdateFarm).Methods("POST")
+    log.Output(2, "Routing /user/farm - Farm operations")
 
     log.Fatal(http.ListenAndServe(":8000", router))
 }
