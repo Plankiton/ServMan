c1f8b6d3f450939ea15f5c041f277fb7d5eb34ed
Plankiton <pl4nk1ton@gmail.com>
2021-01-04 11:54:52 -0300

    Extrai styles da page

--- servman/src/pages/User.js
+++ servman/src/pages/User.js
@@ -11,6 +11,7 @@ import { View,
     StyleSheet} from 'react-native';
 import logo from '../assets/logo.png';
 import { Platform } from '@unimodules/core';
+import styles from '../Styles';
 
 import api from '../services/api'
 export default function User({navigation}) {
@@ -82,7 +83,7 @@ export default function User({navigation}) {
         <KeyboardAvoidingView
          enabled={Platform.OS== 'ios'} 
          behavior="padding" style={styles.container}>
-            <View style={styles.box}>
+            <View style={styles.centerBox}>
                 <Image source={logo}/>
 
                 <View style={styles.form}>
@@ -141,55 +142,3 @@ export default function User({navigation}) {
         </KeyboardAvoidingView>
     )
 }
-const styles = StyleSheet.create({
-   container: {
-       flex:1,
-       justifyContent:'center',
-       alignItems:'center'
-   }, 
-   box:{
-        alignSelf: 'stretch',
-        paddingHorizontal: 30,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginTop: 30,
-   },
-   form:{
-        alignSelf: 'stretch',
-        paddingHorizontal: 30,
-        marginTop: 30,
-        marginBottom: 30,
-   },
-   label: {
-       fontWeight: 'bold',
-       color:'#444',
-       marginBottom:8
-   },
-   input: {
-       borderWidth:1,
-       borderColor: '#ddd',
-       paddingHorizontal:20,
-       fontSize: 16,
-       color:'#444',
-       height: 44,
-       marginBottom: 20,
-       borderRadius: 2
-   },
-   button: {
-       height: 42,
-       backgroundColor: '#23B185',
-       justifyContent: 'center',
-       alignItems:'center',
-       borderRadius:2,
-   },
-   buttonText:{
-       color: '#FFF',
-       fontWeight:'bold',
-       fontSize:16,
-   },
-   linkText: {
-       color: '#23B185',
-       fontWeight:'bold',
-       fontSize:16,
-   },
-});
